890428b5b1461aa0fcfcda54cec19acfc7f1e00f
AzariahSILAS <95987530+AzariahSILAS@users.noreply.github.com>
2025-08-28 13:35:40 -0700

    Im adding the variables and data type file

---
+++ basics/variables_data-types.py
@@ -0,0 +1,69 @@
+# -------------------------variables ------------------------
+name = " Azariah"
+
+age = 24
+
+shopping_list = ['salt', ' bottled water', 'rice']
+
+
+# ------------------------- data types ------------------------
+# numeric
+# sequence
+# set
+# dictionary
+# boolean
+
+
+
+# ------------------------ numeric
+
+#integers
+
+age = 24
+print(age)
+print(dir(age))
+print(help(age))
+
+#floats
+dollar_per_hour = 25.50
+print(dollar_per_hour)
+
+#complex numbers
+
+z = complex(1,3)
+print(z)
+print(z.imag)
+print(z.real)
+
+x = 2+4j
+
+print(x.imag)
+print(x.real)
+
+# ----------------------sequence
+
+#strings
+name = "Azariah"
+print(name)
+print(name[0])
+#list
+storage = ['rifle', 'food', 100, name, 'armer']
+print(storage[3])
+
+#tuples
+
+player = (name, "male", 24)
+print(player)
+
+# ----------------------sets
+
+colors = {'blue', 'green', 'red'}
+print(colors)
+
+
+# ----------------------dictionary
+
+charactor_1 = { "name":"Azariah,", "damage":10, "health":100, "armer":"full" }
+
+
+
