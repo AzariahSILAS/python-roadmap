4db09a6d6acf551069e7f54a9eaae00e474a5876
AzariahSILAS <95987530+AzariahSILAS@users.noreply.github.com>
2025-08-30 14:45:03 -0700

    I added a few new files and some content to my first file

--- basics/1-basics.py
+++ basics/1-basics.py
@@ -1,74 +1,31 @@
-# -------------------------variables ------------------------
-name = " Azariah"
-
-age = 24
-
-shopping_list = ['salt', ' bottled water', 'rice']
-
-
-# ------------------------- data types ------------------------
-# numeric
-# sequence
-# set
-# dictionary
-# boolean
+#comments 
+#variables
+#operators
+#expressions
+#a brief overview of functions
 
 
 
-# ------------------------ numeric
-
-#integers
-
-age = 24
-print(age)
-print(dir(age))
-print(help(age))
-
-#floats
-dollar_per_hour = 25.50
-print(dollar_per_hour)
-
-#complex numbers
-
-z = complex(1,3)
-print(z)
-print(z.imag)
-print(z.real)
-
-x = 2+4j
-
-print(x.imag)
-print(x.real)
-
-# ----------------------sequence
-
-#strings
-name = "Azariah"
-print(name)
-print(name[0])
-#list
-storage = ['rifle', 'food', 100, name, 'armer']
-print(storage[3])
-
-#tuples
-
-player = (name, "male", 24)
-print(player)
-
-# ----------------------sets
-
-colors = {'blue', 'green', 'red'}
-print(colors)
-
-
-# ----------------------dictionary
-
-charactor_1 = { "name":"Azariah", "damage":10, "health":100, "armer":"full" }
-
-charactor_2 = { "name":"Samantha", "damage":10, "health":100, "armer":"full" }
+#variables------------------------
 
 
 
 
 
 
+
+
+#arithmatic operators------------
+
+# + addition
+# - subtraction
+# * multiplication
+# / division(returns a float)
+# // floor division(returns an integer)
+# % modulus (remainder)
+# ** exponent (power)
+#---------------------------------------
+
+
+
+#arithmatic operators------------
