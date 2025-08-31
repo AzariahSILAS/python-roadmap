feef881fd2ede644e5849bc42dec25da52d798f2
AzariahSILAS <95987530+AzariahSILAS@users.noreply.github.com>
2025-08-31 13:01:01 -0700

    I added some content and fifnished the boilerplate

--- basics/1-basics.py
+++ basics/1-basics.py
@@ -1,16 +1,34 @@
+import keyword
+
 #comments 
 #variables
 #operators
-#expressions
+#expressions vs statements
 #a brief overview of functions
 
 
 
 #variables------------------------
+# a variable is a named reference to a value stored in memory -------
+
+# reference = value
+age = 24
+dollar_per_hour = 22.50
+name = "azariah"
 
 
+# rules for naming variables
 
+#must start with a letter (A-Z, a-z) or underscore(_)
 
+# reserved keywords (can't be used for variables names)
+
+# and, as, assert, break, class, continue, def, del,
+# elif, else, except, False, finally, for, from, global,
+# if, import, in, is, lambda, None, nonlocal, not, or,
+# pass, raise, return, True, try, while, with, yield
+
+#print(keyword.kwlist)
 
 
 
@@ -27,6 +45,25 @@
 #---------------------------------------
 
 
+add = 5 + 5
+
+sub = 5 - 1
+
+mult = 6 * 7
+
+div = 5 / 2
+
+fdiv = 5 // 2
+
+mod = 10 % 3
+
+exp = 2 ** 2
+
+
+
+print(fdiv)
+
+
 
 #comparison AKA (relational) operators------------
 
@@ -38,14 +75,28 @@
 # >= (greater or equal to)
 # <= (less then or equal to)
 
+x = 1
+y = 2
+
+z = x >= y
+
+print(z)
+
 
 #logical operators -------------------------------
+
+
 
 # and (True if both are true)
 # or (True if at least one is true)
 # not (True is False, False is True)
 
+x = True
+y = False
 
+z = x or not y
+
+print(z)
 
 #assignment operators ------------------------------------
 
@@ -57,6 +108,11 @@
 # //= (floor devide and assign)
 # %= (modulus and assign)
 # **= (exponent and assign)
+
+x *= 
+
+
+print(x)
 
 #bitwise operators ------------------------------------------
 
@@ -80,3 +136,24 @@
 
 # in (True if value exists in sequence)
 # not (True if value does not exist)
+
+# expressions vs statements-------------------------
+
+#expression (produces a value) (what is x)
+
+x = 5 + 7 #(x is 12)
+
+#statements (performs an action) (do this)
+
+print (x) # (prints the value to the console)
+
+
+
+#basic function ---------------------------------
+
+def sum(a, b):
+    value = a + b
+    print(value)
+
+
+sum(15, 15)
