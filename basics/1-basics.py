2a5148453e0d751c072205853f86ec7d5a9a9e6d
AzariahSILAS <95987530+AzariahSILAS@users.noreply.github.com>
2025-08-31 11:59:56 -0700

    i just added to boilerplate for the basices file, now i need to file it in with code!

--- basics/1-basics.py
+++ basics/1-basics.py
@@ -28,4 +28,55 @@
 
 
 
-#arithmatic operators------------
+#comparison AKA (relational) operators------------
+
+
+# == (equal to)
+# != ( not equal to)
+# > (greater then)
+# < (less then)
+# >= (greater or equal to)
+# <= (less then or equal to)
+
+
+#logical operators -------------------------------
+
+# and (True if both are true)
+# or (True if at least one is true)
+# not (True is False, False is True)
+
+
+
+#assignment operators ------------------------------------
+
+# = (simple assignment)
+# += (add and assign)
+# -= (subtact and assign)
+# *= (multiply and assign)
+# /= (devide and assign)
+# //= (floor devide and assign)
+# %= (modulus and assign)
+# **= (exponent and assign)
+
+#bitwise operators ------------------------------------------
+
+# & (and)
+# | (or)
+# ^ (xor (exclusive or))
+# ~ (not (inverts bits))\
+# << (left shfit)
+# >> (right shift)
+
+
+# identity operators ---------------------------------------
+#-check if two objects are the same in memory--------------
+
+# is (True if both variables point to the same object in memory)
+# is not (true if they point to difrent objects)
+
+
+#membership operators ----------------------
+#-check if a value is in a sequence (list, string, tuple, etc.)
+
+# in (True if value exists in sequence)
+# not (True if value does not exist)
