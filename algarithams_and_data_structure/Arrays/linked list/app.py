71db9131357613b1cb4a7ad915438044f157261b
AzariahSILAS <95987530+AzariahSILAS@users.noreply.github.com>
2025-09-13 17:22:19 -0700

    Update app.py

--- algarithams_and_data_structure/Arrays/linked list/app.py
+++ algarithams_and_data_structure/Arrays/linked list/app.py
@@ -2,16 +2,27 @@ list = {}
 
 def create_list(new_item):
     number = len(list) + 1
-    list[f"{new_item}{number}"] = new_item
-    print(list)
+    temp = ""
+    counter = 0
+    if(number > 0):
+        
+        for i in list:
+            counter += 1
+            if counter == len(list):
+                temp = i
+        list[f"{new_item}"] = temp
+    else:
+          list[new_item] = "foot"
     
 
 while True:
     question = input("what do you want me to do? ")
 
-    if(question == "add this to list"):
+    if(question == "add"):
         new_question = input("what should i add: ")
         create_list(new_question)
+        for item in list:
+            print(f"'{item}':'{list[item]}'")
         print(list)
     elif(question == "leave"):
         break    
